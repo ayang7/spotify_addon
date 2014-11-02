0fc5422e2cec04d8ee06fe84ed12cdd1f2a77d63
Mark Landgrebe <marklandgrebe@Marks-MacBook-Pro-2.local>
2014-11-02 14:26:56 -0600

    Initial Commit
    Commit

---
+++ Playlist Manager/Playlist Manager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Playlist Manager
+//
+//  Created by Mark Landgrebe on 11/2/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Playlist Manager/Playlist Manager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Playlist Manager
+//
+//  Created by Mark Landgrebe on 11/2/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
