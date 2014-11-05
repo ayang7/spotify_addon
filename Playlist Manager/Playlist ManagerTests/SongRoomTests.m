d15a4fe6b7275ff0a2adcc2c0ab5ff3becdefb10
Andrew Yang <andrewyang@Andrew-Yangs-MacBook-Pro.local>
2014-11-04 23:03:53 -0600

    SongRoom Tests

--- Playlist Manager/Playlist ManagerTests/SongRoomTests.m
+++ Playlist Manager/Playlist ManagerTests/SongRoomTests.m
@@ -7,3 +7,40 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <XCTest/XCTest.h>
+
+#import "Song.h"
+#import "User.h"
+#import "SongRoom.h"
+
+
+User *testUser;
+SongRoom *room;
+
+@interface SongRoomTests : XCTestCase
+
+@end
+
+@implementation SongRoomTests
+
+- (void)setUp
+{
+    room = [[SongRoom alloc] init];
+    [super setUp];
+    
+}
+
+- (void)tearDown
+{
+    song = nil;
+}
+
+- (void)addUser : (User *) user{
+    [room addUser:testUser];
+    XCTAssertEqualObjects(SongRoom.users[0], testUser, @"Failed to add user");
+}
+
+@end
+
+
+
